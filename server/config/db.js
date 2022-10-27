aedae0b54783c96b832a6d7ca6bdf273f2784967
dahye213 <dlee220@my.centennialcollege.ca>
2022-10-27 10:32:37 -0400

    Second commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
   //local MongoDB deployment ->
-  URI: "mongodb+srv://dhlee:zebLsTmaIni01fEv@cluster0.dtn4gko.mongodb.net/Emp?retryWrites=true&w=majority",
+  URI: "mongodb+srv://dhlee:Password@cluster0.dtn4gko.mongodb.net/Emp?retryWrites=true&w=majority",
 };
